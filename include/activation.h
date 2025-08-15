893b6f3156d6e865663f9dd729c00b41da58ade8
zephyr <alejandrotorresn@gmail.com>
2025-08-15 07:49:28 -0500

    Esquema Activation

---
+++ include/activation.h
@@ -0,0 +1,8 @@
+//
+// Created by zephyr on 8/14/25.
+//
+
+#ifndef MLP_BASE_ACTIVATION_H
+#define MLP_BASE_ACTIVATION_H
+
+#endif //MLP_BASE_ACTIVATION_H
---
+++ src/activation.cpp
@@ -0,0 +1,3 @@
+//
+// Created by zephyr on 8/14/25.
+//
